c348691c25c3a4d123f2d3178984c4f60ca17307
Dylan Ogle <delxhq0@gmail.com>
2021-03-23 17:45:42 +0000

    Batched?

--- src/bedrock/Disconnect.ts
+++ src/bedrock/Disconnect.ts
@@ -1,12 +1,13 @@
 import { BundledPacket } from '../raknet/BundledPacket'
 import { DataType, Packets } from '../types'
+import { BatchedPacket } from './BatchedPacket'
 
 interface IDisconnect {
   hideScreen: boolean,
   message?: string,
 }
 
-export class Disconnect extends BundledPacket<IDisconnect> {
+export class Disconnect extends BatchedPacket<IDisconnect> {
 
   constructor(p?: IDisconnect) {
     super(Packets.DISCONNECT, [
